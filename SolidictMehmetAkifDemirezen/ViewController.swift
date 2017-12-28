2c213ec031f4dff09363c5d8be7cdc796f7a2b43
akif demirezen <demirezen@akifs-Mac.local>
2017-12-28 21:27:52 +0200

    Initial Commit

---
+++ SolidictMehmetAkifDemirezen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolidictMehmetAkifDemirezen
+//
+//  Created by akif demirezen on 28/12/2017.
+//  Copyright © 2017 demirezenOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
